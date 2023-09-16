0d6184a5df2d5f1062b59aaba03a87a8f4f43e52
MuhammadMangitov <mangitovmuhammad2256@gmail.com>
2023-09-16 19:32:52 +0500

    boldi

--- Lesson01/Lesson01/Program.cs
+++ Lesson01/Lesson01/Program.cs
@@ -11,7 +11,8 @@
         // Complete the following method
         public static int Add(int a, int b)
         {
-            return 0;
+           
+            return a + b;
         }
     }
 }
