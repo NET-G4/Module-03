65e81f09f61d7a5a6715be5f62901c017a026db7
nurmurodoveec <137428953+nurmurodoveec@users.noreply.github.com>
2023-09-16 19:15:01 +0500

    Abduyusupov Davlatshokh01

---
+++ Lesson01/AbduyusupovDavlatshokh-lesson1/AbduyusupovDavlatshokh01/AbduyusupovDavlatshokh01/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AbduyusupovDavlatshokh01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point(10, 10);
+            point.info();
+        }
+    }
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+
+        }
+        public void info()
+        {
+            Console.WriteLine($"X:{X}Y:{Y}");
+        }
+    }
+}
