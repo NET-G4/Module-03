dab8707e1293892a2383d64493b25b9b2c4ee28b
nurmurodoveec <137428953+nurmurodoveec@users.noreply.github.com>
2023-09-16 19:36:48 +0500

    Davlatshokh- review improvment

--- Lesson01/AbduyusupovDavlatshokh-lesson1/AbduyusupovDavlatshokh01/AbduyusupovDavlatshokh01/Program.cs
+++ Lesson01/AbduyusupovDavlatshokh-lesson1/AbduyusupovDavlatshokh01/AbduyusupovDavlatshokh01/Program.cs
@@ -4,23 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Point point = new Point(10, 10);
-            point.info();
-        }
-    }
-    class Point
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-        public Point(int x, int y)
-        {
-            X = x;
-            Y = y;
 
         }
-        public void info()
+
+        public static int Add(int a, int b)
         {
-            Console.WriteLine($"X:{X}Y:{Y}");
+            return a + b;
         }
     }
+
 }
