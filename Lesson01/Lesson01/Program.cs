24367eda49c443e0a67978ec9f7a3c0624980261
aXayitov <137428961+aXayitov@users.noreply.github.com>
2023-09-16 20:34:40 +0500

    xato tuzatildi

--- Lesson01/Lesson01/Program.cs
+++ Lesson01/Lesson01/Program.cs
@@ -7,16 +7,9 @@
             Console.WriteLine("Hello, World!");
         }
 
-        // Homework
-        // Complete the following method
         public static int Add(int a, int b)
         {
-            if (a < b)
-            {
-                Console.WriteLine($"{a} smaller than {b}");
-                return 0;
-            }
-            return a - b;
+            return a + b;
         }
     }
 }
