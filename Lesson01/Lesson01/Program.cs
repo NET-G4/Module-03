cbd7dd7ebffc9f7e58c97429af4f5190214bd166
aXayitov <137428961+aXayitov@users.noreply.github.com>
2023-09-16 19:32:28 +0500

    12122

--- Lesson01/Lesson01/Program.cs
+++ Lesson01/Lesson01/Program.cs
@@ -11,7 +11,12 @@
         // Complete the following method
         public static int Add(int a, int b)
         {
-            return 0;
+            if (a < b)
+            {
+                Console.WriteLine($"{a} smaller than {b}");
+                return 0;
+            }
+            return a - b;
         }
     }
 }
