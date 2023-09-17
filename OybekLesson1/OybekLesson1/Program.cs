25c039c4969d077514ba9cb7f0d55a2f59a3f0f4
uzb001 <137428777+uzb001@users.noreply.github.com>
2023-09-17 17:37:53 +0800

    homeWork1

---
+++ OybekLesson1/OybekLesson1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OybekLesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Xavotirsiz hayot go'zal!");
+            Console.WriteLine("Qaysi yo'l bilan jarimani tekshirmoqchisiz? ");
+            Console.WriteLine("1. Passport.");
+            Console.WriteLine("Tex Passport.");
+            Console.WriteLine("Avtoulov raqami.");
+
+            int SetInfo = int.Parse(Console.ReadLine());
+            if (SetInfo == 1)
+            {
+                string WriteInfo = Console.ReadLine();
+
+            }
+
+        }
+    }
+}
