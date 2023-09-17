b6fc3bca17cef720336afa1384c9f2409277fa16
nurmurodoveec <137428953+nurmurodoveec@users.noreply.github.com>
2023-09-17 14:35:36 +0500

    homework01

---
+++ Lesson01/Davlatshokh_lesson01_hw/Davlatshokh_lesson01_hw/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Davlatshokh_lesson01_hw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Xavotirsiz Hayot go'zal! ");
+            Console.WriteLine("Qanday turda jarimalaringizni tekshirmoqchisiz!");
+            Console.WriteLine("1.Passport card orqali: ");
+            Console.WriteLine("2.Tex passport orqali: ");
+            Console.WriteLine("3.Avto mashina raqami orqali: ");
+
+            int SetInfo=int.Parse(Console.ReadLine());
+
+            string WriteInfo=Console.ReadLine();
+
+    }
+   
+}
---
+++ Lesson01/Davlatshokh_lesson01_hw/Davlatshokh_lesson01_hw/fine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Davlatshokh_lesson01_hw
+{
+    internal class fine
+    {
+        public string PassportID { get; set; }
+        public string TechnicalId { get; set; }
+        public string CarNumber { get; set; }
+
+        public fine(string passportid, string technicalid,string carnumber)
+            {
+            PassportID = passportid;
+            TechnicalId = technicalid;
+            CarNumber = carnumber;
+            }
+        public void passportID(string passportid)
+        {
+            if (PassportID != null) { }
+
+        }
+
+    }
+}
