37452862de491804f3b2174599ac6f4b525886c0
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2023-09-23 16:40:06 +0200

    Add homework implementation

--- Lesson05/Lesson05/Program.cs
+++ Lesson05/Lesson05/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Person person = new Person();
+            Display(person);
+
+            Animal animal = new Animal();
+            Display(animal);
+
+            Cat cat = new Cat();
+            Display(cat);
         }
+
+        static void Display(IRunnable runnable)
+        {
+            runnable.Run();
+        }
+    }
+
+    interface IRunnable
+    {
+        public void Run();
+    }
+
+    class Person : IRunnable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Person Running");
+        }
+    }
+
+    class Animal : IRunnable
+    {
+        public void Run()
+        {
+            Console.WriteLine("Animal running");
+        }
+    }
+
+    class Cat : IRunnable
+    {
     }
 }
